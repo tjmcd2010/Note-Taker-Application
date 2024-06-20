3e75487a941d6cbd2344ca58df2bc0da1882c04c
Travis McDermott <tjmcd2010@gmail.com>
2024-06-20 11:29:47 -0700

    Upates requirements and api calls

--- server.js
+++ server.js
@@ -2,8 +2,26 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const api = require('./routes/index.js');
+const api = require('./public/assets/js/index');
+app.use(clog);
 // Middleware for parsing JSON and urlencoded form data
-app.use(express.static('public'));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+app.use(express.static('public'));
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+// Wildcard route to direct users to a 404 page
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/404.html'))
+);
+app.listen(PORT, () =>
+  console.log(`App listening at XXXXXXXXXXXXXXXX:${PORT}`)
+);
 
